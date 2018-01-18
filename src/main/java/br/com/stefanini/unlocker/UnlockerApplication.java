1aada5b0ac671e59926b70dd763f0befdf0e358d
Eduardo Santos <esbrito@stefanini.com>
2018-01-17 22:01:56 -0200

    Ambientação

---
+++ src/main/java/br/com/stefanini/unlocker/UnlockerApplication.java
@@ -0,0 +1,12 @@
+package br.com.stefanini.unlocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnlockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnlockerApplication.class, args);
+	}
+}
